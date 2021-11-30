5f90840cf6319190e143767e3dfa2373c0e058f8
gantit <gantit@hotmail.com>
2021-11-30 02:09:25 +0100

    feat: init project

---
+++ .eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  env: {
+    node: true,
+  },
+  rules: {
+    "import/order": [
+      "error",
+      {
+        groups: [
+          // 1. node "builtin" modules
+          // ex: 'fs', 'path'
+          "builtin",
+          // 2. "external" modules
+          // ex: 'axios', 'lodash'
+          "external",
+          // 3. "internal" modules
+          // ex: 'src/foo'
+          "internal",
+          // 4. modules from a "parent" directory
+          // ex: '../foo', '../../foo/qux'
+          // AND
+          // "sibling" modules from the same or a sibling's directory
+          // ex: './bar', './bar/baz'
+          ["sibling", "parent"],
+          // 6. "type" imports
+          // ex: import type { Foo } from 'foo'
+          "type",
+          // 5. "index" of the current directory
+          // ex: './'
+          "index",
+        ],
+        pathGroups: [
+          {
+            pattern: "components/**",
+            group: "type",
+            position: "after",
+          },
+          {
+            pattern: "utils/**",
+            group: "type",
+            position: "after",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+};
